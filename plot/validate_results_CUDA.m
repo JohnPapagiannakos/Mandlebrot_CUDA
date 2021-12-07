754f1080ed12eb3573ccea9a2934134105f73ab7
John Papagiannakos <jpapagiannakos@gmail.com>
2021-12-07 15:29:52 +0200

    feat: added matlab code in order to plot resulting fractal

---
+++ plot/validate_results_CUDA.m
@@ -0,0 +1,22 @@
+% Author: Yannis Papagiannakos
+%
+% Read data (bin files) and plot fractal
+
+fid = fopen('../bin/count.bin','r','n');
+count = fread(fid, 'double');
+dims = length(count);
+dims = sqrt(dims);
+count = reshape(count, dims, dims).';
+fclose(fid);
+
+fid = fopen('../bin/x.bin','r','n');
+x = fread(fid, 'double');
+fclose(fid);
+
+fid = fopen('../bin/y.bin','r','n');
+y = fread(fid, 'double');
+fclose(fid);
+
+figure()
+imagesc( x, y, count );
+colormap( [jet();flipud( jet() );0 0 0] );axis off
