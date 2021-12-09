cae7c3285b2d35a12c7ebd78809e79c48ca74bab
John Papagiannakos <jpapagiannakos@gmail.com>
2021-12-09 21:52:35 +0200

    refactor: renamed files in script

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,6 @@
 #--- requires cuda v. 11.0 or later (in order to add -std=c++17)---.
 
-nvcc --gpu-architecture=sm_75 -lcudart -lcuda -lcublas ../include/operators.cu -O3 -c -o ../bin/operators.o
+nvcc --gpu-architecture=sm_75 -lcudart -lcuda -lcublas ../include/cudaoperators.cu -O3 -c -o ../bin/cudaoperators.o
 
 nvcc -L/usr/local/cuda-11.2/lib64 --gpu-architecture=sm_75 -lcudart -lcuda -std=c++17\
-     -I../include ../bin/operators.o test_julia.cpp -Xcompiler "-O3 -march=native -mtune=native" -o ../bin/test_julia
+     -I../include ../bin/cudaoperators.o test_julia.cu -Xcompiler "-O3 -march=native -mtune=native" -o ../bin/test_julia
