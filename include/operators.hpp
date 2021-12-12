27a0c90c869cf53f43dedadea2252f9a1f8e499f
John Papagiannakos <jpapagiannakos@gmail.com>
2021-12-12 03:18:43 +0200

    feat: improved performance of openmp by 300%. added namespaces for each version

--- include/operators.hpp
+++ include/operators.hpp
@@ -3,6 +3,10 @@
 #include <iomanip>
 #include <cmath>
 #include <types.hpp>
+#include <vector>
+#include <numeric>
+#include <string>
+#include <functional>
 
 #define CHUNKSIZE 64
 
@@ -41,49 +45,147 @@ inline void z2(DoubleComplex &z, const DoubleComplex c)
 
 inline void logv(double *a, int length)
 {
-    #pragma omp parallel for schedule(static, CHUNKSIZE)
+    #pragma omp for schedule(static, CHUNKSIZE)
     for (int idx = 0; idx < length; idx++)
     {
         a[idx] = std::log(a[idx]);
     }
 }
 
-inline void juliaop2(DoubleComplex *z, const DoubleComplex c, double *count, int length)
+
+namespace v1 //slow
 {
-    #pragma omp parallel for schedule(static, CHUNKSIZE)
-    for(int idx=0; idx<length; idx++)
+    inline void juliaop2(DoubleComplex *z, const DoubleComplex c, double *count, int length)
     {
-        z2(z[idx], c);
-        double margin = margind(z[idx]);
-        count[idx] += static_cast<double>(margin <= 2);
+    #pragma omp for schedule(static, CHUNKSIZE)
+        for (int idx = 0; idx < length; idx++)
+        {
+            z2(z[idx], c);
+            double margin = margind(z[idx]);
+            count[idx] += static_cast<double>(margin <= 2);
+        }
+    }
+
+    inline void juliaop3(DoubleComplex *z, const DoubleComplex c, double *count, int length)
+    {
+    #pragma omp for schedule(static, CHUNKSIZE)
+        for (int idx = 0; idx < length; idx++)
+        {
+            z3(z[idx], c);
+            double margin = margind(z[idx]);
+            count[idx] += static_cast<double>(margin <= 2);
+        }
+    }
+
+
+    void JuliaOp2(DoubleComplex *z, const DoubleComplex c, double *count, int length, const int MAX_ITERS)
+    {
+        #pragma omp parallel
+        {
+            for (int iter = 0; iter <= MAX_ITERS; iter++)
+            {
+                juliaop2(z, c, count, length);
+            }
+            logv(count, length);
+        }
+    }
+
+    void JuliaOp3(DoubleComplex *z, const DoubleComplex c, double *count, int length, const int MAX_ITERS)
+    {
+    #pragma omp parallel
+            {
+                for (int iter = 0; iter <= MAX_ITERS; iter++)
+                {
+                    juliaop3(z, c, count, length);
+                }
+                logv(count, length);
+            }
+        }
+}
+
+inline namespace v2 // fast
+{
+    inline void juliaop2(DoubleComplex *z, const DoubleComplex c, double *count, int length, const int MAX_ITERS)
+    {
+        #pragma omp for schedule(static, CHUNKSIZE)
+        for (int idx = 0; idx < length; idx++)
+        {
+            for (int iter = 0; iter <= MAX_ITERS; iter++)
+            {
+                z2(z[idx], c);
+                double margin = margind(z[idx]);
+                count[idx] += static_cast<double>(margin <= 2);
+            }
+        }
+    }
+
+    inline void juliaop3(DoubleComplex *z, const DoubleComplex c, double *count, int length, const int MAX_ITERS)
+    {
+        #pragma omp for schedule(static, CHUNKSIZE)
+        for (int idx = 0; idx < length; idx++)
+        {
+            for (int iter = 0; iter <= MAX_ITERS; iter++)
+            {
+                z3(z[idx], c);
+                double margin = margind(z[idx]);
+                count[idx] += static_cast<double>(margin <= 2);
+            }
+        }
+    }
+
+
+    void JuliaOp2(DoubleComplex *z, const DoubleComplex c, double *count, int length, const int MAX_ITERS)
+    {
+        #pragma omp parallel
+        {
+            juliaop2(z, c, count, length, MAX_ITERS);
+            logv(count, length);
+        }
+    }
+
+    void JuliaOp3(DoubleComplex *z, const DoubleComplex c, double *count, int length, const int MAX_ITERS)
+    {
+        #pragma omp parallel
+        {
+            juliaop3(z, c, count, length, MAX_ITERS);
+            logv(count, length);
+        }
     }
 }
 
-inline void juliaop3(DoubleComplex *z, const DoubleComplex c, double *count, int length)
+namespace rfc // if values of matrix count do not change stop algorithm 
 {
-    #pragma omp parallel for schedule(static, CHUNKSIZE)
-    for (int idx = 0; idx < length; idx++)
+    int JuliaOp2(DoubleComplex *z, const DoubleComplex c, double *count, int length)
     {
-        z3(z[idx], c);
-        double margin = margind(z[idx]);
-        count[idx] += static_cast<double>(margin <= 2);
+        double f_val_prev = 0, f_val_curr = 0;
+        int iter = 0;
+        while(1)
+        {
+            v1::juliaop2(z, c, count, length);
+            f_val_prev = f_val_curr;
+            f_val_curr = std::accumulate(&count[0], &count[length - 1], 0);
+            if (std::abs(f_val_prev - f_val_curr) <= 1)
+                break;
+            iter++;
+        }
+        logv(count, length);
+        return iter;
     }
-}
 
-void JuliaOp2(DoubleComplex *z, const DoubleComplex c, double *count, int length, const int MAX_ITERS)
-{
-    for (int iter = 0; iter <= MAX_ITERS; iter++)
+    int JuliaOp3(DoubleComplex *z, const DoubleComplex c, double *count, int length)
     {
-        juliaop2(z, c, count, length);
+        double f_val_prev = 0, f_val_curr = 0;
+        int iter = 0;
+        while (1)
+        {
+            v1::juliaop3(z, c, count, length);
+            f_val_prev = f_val_curr;
+            f_val_curr = std::accumulate(&count[0], &count[length - 1], 0);
+            if (std::abs(f_val_prev - f_val_curr) <= 1)
+                break;
+            iter++;
+        }
+        logv(count, length);
+        return iter;
     }
-    logv(count, length);
-}
-
-void JuliaOp3(DoubleComplex *z, const DoubleComplex c, double *count, int length, const int MAX_ITERS)
-{
-    for (int iter = 0; iter <= MAX_ITERS; iter++)
-    {
-        juliaop3(z, c, count, length);
-    }
-    logv(count, length);
 }
